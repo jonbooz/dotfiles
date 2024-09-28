4db17e1b7938797b2aba6a7cc89470393a8e2fb8
Jonathan Booz <jon@jonbooz.com>
2024-09-28 08:39:26 -0400

    Add script (.bin/) support

--- install/copy.sh
+++ install/copy.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 echo "Creating required directories..."
+mkdir -p ~/.bin/
 mkdir -p ~/.config/nvim/
 mkdir -p ~/.config/zsh
 
@@ -11,3 +12,5 @@ echo "Copying: zshrc -> ~/.zshrc" && cp zshrc ~/.zshrc
 echo "Copying: zsh/ -> ~/.config/zsh" && rsync -avhH --delete zsh/ ~/.config/zsh/
 
 echo "Copying: tmux.conf -> ~/.tmux.conf" && cp tmux.conf ~/.tmux.conf
+
+echo "Copying: bin/  -> ~/.bin/" && rsync -avhH --delete bin/ ~/.bin/
--- zsh/path-mac.zsh
+++ zsh/path-mac.zsh
@@ -3,4 +3,4 @@ export PATH=$PATH:$HOME/.bin
 export PATH=${PATH}:$(find $HOME/.bin -type d | tr '\n' ':' | sed 's/:$//')
 export PATH=/usr/local/bin:$PATH
 export PATH=$PATH:/opt/homebrew/bin/
-
+export PATH=$PATH:$HOME/.bin/local
