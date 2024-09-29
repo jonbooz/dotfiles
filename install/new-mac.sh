33aff7773a5195c5f738506b20e2a1446ed2e04f
Jonathan Booz <jon@jonbooz.com>
2024-09-28 21:28:13 -0400

    Move brew install command to Brewfile

--- install/new-mac.sh
+++ install/new-mac.sh
@@ -1,22 +1,10 @@
 #!/bin/zsh
 
-echo "Installing homebrew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# echo "Installing homebrew..."
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install mas font-hack-nerd-font
 
 echo "Installing dependencies..." && \
-    /opt/homebrew/bin/brew install \
-            neovim \
-            cmake \
-            fd \
-            ripgrep \
-            font-hack-nerd-font \
-            tmux \
-            fzf \
-            bat \
-            eza \
-            btop \
-            glow
-
-brew install --cask amethyst
-
+    /opt/homebrew/bin/brew bundle --file ./install/Brewfile
 
