384e5a884fa0869a8e86d6ac34ba66fd1395a0dd
Jonathan Booz <jon@jonbooz.com>
2024-09-28 21:40:31 -0400

    Re-enable install homebrew

--- install/new-mac.sh
+++ install/new-mac.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-# echo "Installing homebrew..."
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "Installing homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install mas font-hack-nerd-font
 
