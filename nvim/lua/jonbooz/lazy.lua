13591270398baa7742705f8d810edd9cbce4b6ef
Jon Booz <jwb@jonbooz.com>
2025-04-18 23:13:14 -0400

    Update telescope-fzf build command

--- nvim/lua/jonbooz/lazy.lua
+++ nvim/lua/jonbooz/lazy.lua
@@ -15,7 +15,7 @@ vim.opt.rtp:prepend(lazypath)
 
 
 local plugins = {
-    { 'nvim-telescope/telescope-fzf-native.nvim',   build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+    { 'nvim-telescope/telescope-fzf-native.nvim',   build = 'cmake -S. -Bbuild -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
     { 'nvim-telescope/telescope.nvim',              tag = '0.1.8',                                                                                                                         dependencies = { 'nvim-lua/plenary.nvim' } },
 
     { "JoosepAlviste/nvim-ts-context-commentstring" },
