cdc7bb5269284a086f9bbdbc9bb6f1ce99d7b45f
Jon Booz <jwb@jonbooz.com>
2025-08-15 16:51:08 -0400

    Use full brew path in install/new-mac

--- install/new-mac.sh
+++ install/new-mac.sh
@@ -3,7 +3,7 @@
 echo "Installing homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install mas font-hack-nerd-font
+/opt/homebrew/bin/brew install mas font-hack-nerd-font
 
 echo "Installing dependencies..." && \
     /opt/homebrew/bin/brew bundle --file ./install/Brewfile
