40ee21dcd5dc036690e855d1a4615dce6e778f70
Jon Booz <jwb@jonbooz.com>
2026-05-16 23:10:59 -0400

    Update nvim tab completion to complete on tab

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,11 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = { preset = "super-tab" },
+    completion = {
+      list = {
+        selection = { preselect = false, auto_insert = true },
+      },
+    },
+  },
+}
